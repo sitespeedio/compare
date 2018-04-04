e2ad1e6c32a7db68e68bf9ecb904b48dbc673a49
Peter Hedenskog <phedenskog@wikimedia.org>
2018-04-04 09:28:09 +0200

    Refactor main JS into multiple files and simplify upload.

---
+++ js/compare-loadfiles.js
@@ -0,0 +1,171 @@
+/* global Zlib */
+/* exported readHar, fetchHar, getHarURL*/
+
+/**
+ * Help functions to read HAR files from file
+ * or from URL
+ */
+
+function gzipArrayBufferToJSON(arrayBuffer) {
+  /* utf.js - UTF-8 <=> UTF-16 convertion
+   *
+   * Copyright (C) 1999 Masanao Izumo <iz@onicos.co.jp>
+   * Version: 1.0
+   * LastModified: Dec 25 1999
+   * This library is free.  You can redistribute it and/or modify it.
+   */
+
+  function Utf8ArrayToStr(array) {
+    let out, i, len, c;
+    let char2, char3;
+
+    out = '';
+    len = array.length;
+    i = 0;
+    while (i < len) {
+      c = array[i++];
+      switch (c >> 4) {
+        case 0:
+        case 1:
+        case 2:
+        case 3:
+        case 4:
+        case 5:
+        case 6:
+        case 7:
+          // 0xxxxxxx
+          out += String.fromCharCode(c);
+          break;
+        case 12:
+        case 13:
+          // 110x xxxx   10xx xxxx
+          char2 = array[i++];
+          out += String.fromCharCode(((c & 0x1f) << 6) | (char2 & 0x3f));
+          break;
+        case 14:
+          // 1110 xxxx  10xx xxxx  10xx xxxx
+          char2 = array[i++];
+          char3 = array[i++];
+          out += String.fromCharCode(
+            ((c & 0x0f) << 12) | ((char2 & 0x3f) << 6) | ((char3 & 0x3f) << 0)
+          );
+          break;
+      }
+    }
+
+    return out;
+  }
+
+  const byteArray = new Uint8Array(arrayBuffer);
+  const gunzip = new Zlib.Gunzip(byteArray);
+  const decompressedArray = gunzip.decompress();
+  let string = '';
+  // only way to make it work on Safari iOS?
+  try {
+    string = new TextDecoder('utf-8').decode(decompressedArray);
+  } catch (e) {
+    string = Utf8ArrayToStr(decompressedArray);
+  }
+  return JSON.parse(string);
+}
+
+function isFileGzipped(url) {
+  return url.endsWith('.gz');
+}
+
+function isFileZipped(url) {
+  return url.endsWith('.zip') || url.endsWith('.zhar');
+}
+
+function readGZipFile(file) {
+  return new Promise((resolve, reject) => {
+    const reader = new FileReader();
+
+    reader.onerror = () =>
+      reject(
+        new Error('Error reading ' + file.name + ' : ' + reader.error.name)
+      );
+    reader.onload = () => {
+      try {
+        const har = gzipArrayBufferToJSON(reader.result);
+        resolve(har);
+      } catch (e) {
+        reject(new Error('Error reading ' + file.name + ' : ' + e.message));
+      }
+    };
+
+    reader.readAsArrayBuffer(file.nativeFile ? file.nativeFile : file);
+  });
+}
+
+function readJsonFile(file) {
+  return new Promise((resolve, reject) => {
+    const reader = new FileReader();
+
+    reader.onerror = () =>
+      reject(
+        new Error('Error reading ' + file.name + ' : ' + reader.error.message)
+      );
+    reader.onload = () => {
+      try {
+        const har = JSON.parse(reader.result);
+        resolve(har);
+      } catch (e) {
+        reject(new Error('Error reading ' + file.name + ' : ' + e.message));
+      }
+    };
+    reader.readAsText(file.nativeFile ? file.nativeFile : file);
+  });
+}
+/**
+ * Magically calculate the URL from a sitespeed.io upload
+ * @param {string} url
+ */
+function getHarURL(url) {
+  let run = 0,
+    harURL;
+
+  // The sitespeed.io summary page
+  if (url.endsWith('/') || url.endsWith('/index.html')) {
+    run = 0;
+    // TODO we are not sure the HAR file is gzipped, hmm
+    harURL = url.slice(0, url.lastIndexOf('/')) + '/data/browsertime.har.gz';
+  } else if (url.endsWith('.html')) {
+    // Individual run page
+    run = url.slice(url.lastIndexOf('/') + 1, url.length - 5);
+    harURL = url.slice(0, url.lastIndexOf('/')) + '/data/browsertime.har.gz';
+  } else {
+    harURL = url;
+  }
+  return {
+    har: harURL,
+    run: run
+  };
+}
+
+function fetchHar(url) {
+  return fetch(url).then(response => {
+    if (!response.ok)
+      throw new Error(
+        'Failed to fetch har from ' + url + '. Error: ' + response.statusText
+      );
+
+    if (isFileZipped(url))
+      throw new Error('Zip compressed HARs are not supported: ' + url);
+
+    if (isFileGzipped(url))
+      return response.arrayBuffer().then(gzipArrayBufferToJSON);
+
+    return response.json();
+  });
+}
+
+function readHar(file) {
+  if (isFileZipped(file.name))
+    return Promise.reject(
+      new Error('Zip compressed HARs are not supported: ' + file.name)
+    );
+
+  if (isFileGzipped(file.name)) return readGZipFile(file);
+  return readJsonFile(file);
+}
---
+++ js/compare-upload.js
@@ -0,0 +1,105 @@
+/* global readHar, errorMessage, generate, showUpload, removeAndHide, FileDrop */
+/* exported createMainDropZone, createUpload  */
+
+/**
+ * The main drop zone on the start page. Can handle one or two
+ * HAR files.
+ * @param {string} id
+ */
+function createMainDropZone(id) {
+  const zone = new FileDrop(id, {});
+  zone.multiple(true);
+
+  zone.event('send', function(files) {
+    if (files.length > 2) {
+      errorMessage('You can only compare max two HAR files at a time!');
+    } else if (files.length === 2) {
+      Promise.all([readHar(files[0]), readHar(files[1])])
+        .then(([har1, har2]) =>
+          generate(
+            {
+              har: har1,
+              run: 0
+            },
+            {
+              har: har2,
+              run: 0
+            }
+          )
+        )
+        .catch(e => {
+          /* eslint-disable no-console */
+          console.error(e);
+          /* eslint-disable no-console */
+          errorMessage(e.message);
+          showUpload();
+        });
+    } else {
+      readHar(files[0])
+        .then(har => {
+          removeAndHide();
+          generate(
+            {
+              har: har,
+              run: 0
+            },
+            {
+              har: har,
+              run: har.log.pages.length > 1 ? 1 : 0
+            }
+          );
+        })
+        .catch(e => {
+          /* eslint-disable no-console */
+          console.error(e);
+          /* eslint-disable no-console */
+          errorMessage(e.message);
+          showUpload();
+        });
+    }
+  });
+}
+
+/**
+ *
+ * The upload button
+ * @param {string} id
+ */
+function createUpload(id) {
+  const upload = document.getElementById(id);
+
+  upload.addEventListener('change', function() {
+    const files = this.files;
+    if (files.length > 1) {
+      errorMessage('You can only add one HAR at a time');
+    } else {
+      readHar(files[0])
+        .then(har => {
+          const changeHar1 = id.indexOf('har1') > -1;
+          const optionName = changeHar1 ? 'run2Option' : 'run1Option';
+          const e2 = document.getElementById(optionName);
+          const run = e2 ? e2.options[e2.selectedIndex].value : 0;
+          const har1 = changeHar1 ? har : window.har1;
+          const har2 = changeHar1 ? window.har2 : har;
+          const run1 = changeHar1 ? 0 : run;
+          const run2 = changeHar1 ? run : 0;
+          generate(
+            {
+              har: har1,
+              run: run1
+            },
+            {
+              har: har2,
+              run: run2
+            }
+          );
+        })
+        .catch(e => {
+          /* eslint-disable no-console */
+          console.error(e);
+          /* eslint-disable no-console */
+          errorMessage(e.message);
+        });
+    }
+  });
+}
