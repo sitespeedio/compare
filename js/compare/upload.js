2cda74642f9d66cbde4368165aea93c4b9461e3d
Peter Hedenskog <phedenskog@wikimedia.org>
2018-10-19 22:22:35 +0200

    First restructure to be able to release 1.0

--- js/compare/upload.js
+++ js/compare/upload.js
@@ -1,4 +1,4 @@
-/* global readHar, errorMessage, generate, showUpload, removeAndHide, FileDrop */
+/* global readHar, errorMessage, generate, showUpload, showLoading, removeAndHide, FileDrop */
 /* exported createMainDropZone, createUpload  */
 
 /**
@@ -14,47 +14,53 @@ function createMainDropZone(id) {
     if (files.length > 2) {
       errorMessage('You can only compare max two HAR files at a time!');
     } else if (files.length === 2) {
+      showLoading();
       Promise.all([readHar(files[0]), readHar(files[1])])
         .then(([har1, har2]) =>
-          generate(
-            {
+          generate({
+            har1: {
               har: har1,
-              run: 0
+              run: 0,
+              label: 'HAR1'
             },
-            {
+            har2: {
               har: har2,
-              run: 0
+              run: 0,
+              label: 'HAR2'
             }
-          )
+          })
         )
         .catch(e => {
           /* eslint-disable no-console */
           console.error(e);
           /* eslint-disable no-console */
-          errorMessage(e.message);
           showUpload();
+          errorMessage(e.message);
         });
     } else {
+      showLoading();
       readHar(files[0])
         .then(har => {
           removeAndHide();
-          generate(
-            {
+          generate({
+            har1: {
               har: har,
-              run: 0
+              run: 0,
+              label: 'HAR1'
             },
-            {
+            har2: {
               har: har,
-              run: har.log.pages.length > 1 ? 1 : 0
+              run: har.log.pages.length > 1 ? 1 : 0,
+              label: 'HAR2'
             }
-          );
+          });
         })
         .catch(e => {
           /* eslint-disable no-console */
           console.error(e);
           /* eslint-disable no-console */
-          errorMessage(e.message);
           showUpload();
+          errorMessage(e.message);
         });
     }
   });
@@ -79,25 +85,25 @@ function createUpload(id) {
           const optionName = changeHar1 ? 'run2Option' : 'run1Option';
           const e2 = document.getElementById(optionName);
           const run = e2 ? e2.options[e2.selectedIndex].value : 0;
-          const har1 = changeHar1 ? har : window.har1;
-          const har2 = changeHar1 ? window.har2 : har;
+          const har1 = changeHar1 ? har : window.har.har1.har;
+          const har2 = changeHar1 ? window.har.har2.har : har;
           const run1 = changeHar1 ? 0 : run;
           const run2 = changeHar1 ? run : 0;
-          const label1 = changeHar1 ? 'HAR1' : window.label1;
-          const label2 = changeHar1 ? window.label2 : 'HAR2';
+          const label1 = changeHar1 ? 'HAR1' : window.har.har1.label;
+          const label2 = changeHar1 ? window.har.har2.label : 'HAR2';
 
-          generate(
-            {
+          generate({
+            har1: {
               har: har1,
               run: run1,
               label: label1
             },
-            {
+            har2: {
               har: har2,
               run: run2,
               label: label2
             }
-          );
+          });
         })
         .catch(e => {
           /* eslint-disable no-console */
