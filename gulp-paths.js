7e5e4ab45b23c865db7705a218280f5751d4ca4d
Matt Hobbs <matt@nooshu.com>
2018-10-21 23:42:39 +0100

    Update Gulp to version 4, split gulpfile into individual tasks

---
+++ gulp-paths.js
@@ -0,0 +1,12 @@
+module.exports = {
+  paths: {
+    src: {
+      img: 'img/',
+      index: 'index.html'
+    },
+    dist: {
+      build: 'build/',
+      img: 'build/img/'
+    }
+  }
+};
---
+++ gulp-tasks/usemin.js
@@ -0,0 +1,30 @@
+'use strict';
+/**
+ * Usemin task for replaces references to non-optimized scripts
+ * or stylesheets into a set of HTML files
+ */
+const gulp = require('gulp');
+const usemin = require('gulp-usemin');
+const htmlmin = require('gulp-htmlmin');
+const rev = require('gulp-rev');
+const gulpPaths = require('../gulp-paths');
+
+function useminTask(){
+  return gulp
+    .src(gulpPaths.paths.src.index)
+    .pipe(
+      usemin({
+        jsAttributes: {
+          defer: true
+        },
+        css: [rev()],
+        html: [htmlmin({
+          collapseWhitespace: true
+        })],
+        js: [rev()]
+      })
+    )
+    .pipe(gulp.dest(gulpPaths.paths.dist.build));
+}
+
+gulp.task('usemin', useminTask);
