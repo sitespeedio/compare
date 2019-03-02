c0da9e22a1b88a5206b8273f55d6b07c3d865553
Matt Hobbs <matt@nooshu.com>
2019-03-02 22:40:42 +0000

    Update to Workbox 4.0.0 for SW generation

--- gulp-tasks/generate-service-worker.js
+++ gulp-tasks/generate-service-worker.js
@@ -8,19 +8,43 @@ const gulpPaths = require('../gulp-paths');
 
 const swSetup = {
   // set cache name prefix
-  cacheId: 'sitespeed.io-compare-',
+  cacheId: 'sitespeed.io-compare-html-',
   // only allow caching of files under 1MB
   maximumFileSizeToCacheInBytes: 1 * 1024 * 1024,
   // use the a local copy of workbox (no the CDN)
   importWorkboxFrom: 'local',
   // what directory / files to precache
   globDirectory: gulpPaths.paths.dist.build,
-  globPatterns: ['**\/*.{html,js,css,png,jpg,jpeg,ico}'],
+  globPatterns: ['**\/*.html'],
   swDest: `${gulpPaths.paths.dist.build}service-worker.js`,
   // claim any browser tabs that are running immediately
   clientsClaim: true,
   // skip the wait and init immediately
-  skipWaiting: true
+  skipWaiting: true,
+  // remove outdated caches from WorkBox 3.x.x
+  cleanupOutdatedCaches: true,
+  // define custom caching routes for more flexibility
+  runtimeCaching: [{
+    urlPattern: new RegExp('.*.(?:css|js)'),
+    // using cache first strategy since files are revisioned
+    handler: 'CacheFirst',
+    options: {
+      cacheName: 'sitespeed.io-compare-application-cache-',
+      expiration: {
+        // assets cached for 30 days ensuring older revisions clear out after this time
+        maxAgeSeconds: 30 * 24 * 60 * 60
+      }
+    }
+  },
+  {
+    urlPattern: new RegExp('.*.(?:png|jpg|jpeg|ico)'),
+    // StaleWhileRevalidate used for minor assets, cache used first then updated later if required
+    handler: 'StaleWhileRevalidate',
+    options: {
+      cacheName: 'sitespeed.io-compare-image-cache-'
+    }
+  }
+]
 };
 
 function generateTask() {
