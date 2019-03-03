48b0149eba522f056a4093cebc2ee26bdd68cc22
Matt Hobbs <matt@nooshu.com>
2019-03-03 01:15:28 +0000

    Fix linting issues

--- js/compare/update-available.js
+++ js/compare/update-available.js
@@ -1,3 +1,5 @@
+/* exported updateInit */
+
 /**
  * JavaScript functionality for the Service Worker update available prompt
  */
@@ -5,19 +7,19 @@ const elementCache = {
   dialog: document.querySelector('.update-available'),
   reloadButton: document.querySelector('.update-reload'),
   dismissButton: document.querySelector('.update-dismiss')
-}
+};
 
 function showUpdateDialog() {
   elementCache.dialog.classList.add('update-visible');
 }
 
 function addUpdateEvents() {
-  elementCache.dismissButton.addEventListener('click', (evt) => {
+  elementCache.dismissButton.addEventListener('click', evt => {
     evt.preventDefault();
     elementCache.dialog.classList.remove('update-visible');
   });
 
-  elementCache.reloadButton.addEventListener('click', (evt) => {
+  elementCache.reloadButton.addEventListener('click', evt => {
     evt.preventDefault();
     window.location.reload();
   });
